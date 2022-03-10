e36a540b8cdb24747ae089b0027cd76c2e68a88b
Abdelouahed Ait-essaih <a.aitessaih@ayouris.net>
2022-03-10 21:53:21 +0100

    Delete key

--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGOURI:"mongodb+srv://abdelouahed:m8nwyXwvds8lrSEC@cluster1.kes0t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-}
+  MONGOURI:
+    "mongodb+srv://abdelouahed:XXXXXXXXXX@cluster1.kes0t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+};
